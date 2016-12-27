f7347408649cdbd209005b6df6200dd1c72fb2dc
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-27 11:17:03 +0000

    Added skeleton constructors to ContactImpl

--- ContactImpl.java
+++ ContactImpl.java
@@ -3,4 +3,30 @@
  */
 public class ContactImpl{
 
+    private int id;
+    private String name;
+    private String notes;
+
+    public ContactImpl(int id, String name){
+        setId(id);
+        setName(name);
+    }
+
+    public ContactImpl(int id, String name, String notes){
+        this(id, name);
+        setNotes(notes);
+    }
+
+    private void setId(int id){
+        this.id = id;
+    }
+
+    private void setName(String name){
+        this.name = name;
+    }
+
+    private void setNotes(String notes){
+        this.notes = notes;
+    }
+
 }
