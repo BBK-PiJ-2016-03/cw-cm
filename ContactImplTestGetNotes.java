1e2bffe41de4cd3c6702c0b9bb4bec6e4b1b4692
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-27 12:22:04 +0000

    Added tests for getNotes()

--- ContactImplTestGetNotes.java
+++ ContactImplTestGetNotes.java
@@ -4,8 +4,61 @@ import static org.junit.Assert.*;
  * Created by Alexander Worton on 27/12/2016.
  */
 public class ContactImplTestGetNotes {
-    @Test
-    public void getNotesTest(){
+    private ContactImpl contact;
 
+    @Test
+    public void getNotesTestSingle(){
+        String name = "Test";
+        String notes = "Test";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpaced(){
+        String name = "Test";
+        String notes = "Test Test";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpacedMultiple(){
+        String name = "Test";
+        String notes = "Test Test Test Test Test";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestEmpty(){
+        String name = "Test";
+        String notes = "";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpecialChars(){
+        String name = "Test";
+        String notes = "*&^%£\"'@";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestNewLine(){
+        String name = "Test";
+        String notes = "A\nB\nC";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestTab(){
+        String name = "Test";
+        String notes = "A\tB\tC";
+        contact = new ContactImpl(1, name, notes);
+        assertEquals(name, contact.getName());
     }
 }
