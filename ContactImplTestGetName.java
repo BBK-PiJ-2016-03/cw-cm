cf38893cb02c393726c4ae971feda1ace33b26d9
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-27 11:48:45 +0000

    Implemented tests, fail due to unimplemented getName() method

--- ContactImplTestGetName.java
+++ ContactImplTestGetName.java
@@ -1,11 +1,44 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by Alexander Worton on 27/12/2016.
  */
 public class ContactImplTestGetName {
-    @Test
-    public void getNameTest(){
 
+    private ContactImpl contact;
+
+    @Test
+    public void getNameTestSingle(){
+        String name = "Test";
+        contact = new ContactImpl(0, name);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpaced(){
+        String name = "Test Test";
+        contact = new ContactImpl(0, name);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpacedMultiple(){
+        String name = "Test Test Test Test Test";
+        contact = new ContactImpl(0, name);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestEmpty(){
+        String name = "";
+        contact = new ContactImpl(0, name);
+        assertEquals(name, contact.getName());
+    }
+
+    @Test
+    public void getNameTestSpecialChars(){
+        String name = "*&^%£\"'@";
+        contact = new ContactImpl(0, name);
+        assertEquals(name, contact.getName());
     }
 }
