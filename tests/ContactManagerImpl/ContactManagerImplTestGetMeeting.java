baa8a3930207b7a4f902fa773688d84483a67ba3
Alexander Worton <aworto01@mail.bkk.ac.uk>
2017-01-01 13:32:44 +0000

    Implemented getMeeting tests

--- tests/ContactManagerImpl/ContactManagerImplTestGetMeeting.java
+++ tests/ContactManagerImpl/ContactManagerImplTestGetMeeting.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by Alexander Worton on 29/12/2016.
@@ -21,11 +22,21 @@ public class ContactManagerImplTestGetMeeting {
      */
 
     @Test
-    public void testNotes(){
-        String note = "These are the Notes";
+    public void testGetMeetingExisting(){
+        String note = "note";
         int id = data.manager.addNewPastMeeting(data.populatedSet, data.pastDate, note);
 
         PastMeeting meeting = (PastMeeting)data.manager.getMeeting(id);
+        assertTrue(meeting.getContacts().equals(data.populatedSet));
+        assertTrue(meeting.getDate().equals(data.pastDate));
         assertTrue(meeting.getNotes().equals(note));
+    }
+
+    @Test
+    public void testGetMeetingNonExisting(){
+        data.manager = new ContactManagerImpl();
+
+        PastMeeting meeting = (PastMeeting)data.manager.getMeeting(4);
+        assertNull(meeting);
     }
 }
