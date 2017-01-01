f8d92aa831a18c6535ac336ae25e71631bc66b85
Alexander Worton <aworto01@mail.bkk.ac.uk>
2017-01-01 16:03:05 +0000

    Implemented getFutureMeetingList tests

--- tests/ContactManagerImpl/ContactManagerImplTestGetFutureMeetingList.java
+++ tests/ContactManagerImpl/ContactManagerImplTestGetFutureMeetingList.java
@@ -1,3 +1,10 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Alexander Worton on 29/12/2016.
@@ -5,9 +12,16 @@
 public class ContactManagerImplTestGetFutureMeetingList {
 
     private ContactManagerImplTestData data;
+    private Contact selectedContact;
+    private Contact unSelectedContact;
+    private Contact nonExistContact;
+    private Set<Contact> excludedSet;
 
     {
         data = new ContactManagerImplTestData();
+        selectedContact = (Contact)data.manager.getContacts(5).toArray()[0];
+        unSelectedContact = (Contact)data.manager.getContacts(3).toArray()[0];
+        nonExistContact = new ContactImpl(Integer.MAX_VALUE, "I Don't Exist");
     }
 
     /**
@@ -22,4 +36,40 @@ public class ContactManagerImplTestGetFutureMeetingList {
      * @throws IllegalArgumentException if the contact does not exist
      * @throws NullPointerException if the contact is null
      */
+
+    @Before
+    public void before(){
+        excludedSet = data.populatedSet.stream()
+                .filter(e -> !e.equals(selectedContact))
+                .collect(Collectors.toSet());
+
+        data.manager.addFutureMeeting(data.populatedSet, data.futureDate);
+        data.manager.addFutureMeeting(excludedSet, data.futureDate);
+        data.manager.addFutureMeeting(data.populatedSet, data.futureDate);
+        data.manager.addFutureMeeting(data.populatedSet, data.futureDate);
+        data.manager.addFutureMeeting(excludedSet, data.futureDate);
+        data.manager.addFutureMeeting(data.populatedSet, data.futureDate);
+        data.manager.addFutureMeeting(excludedSet, data.futureDate);
+        data.manager.addFutureMeeting(data.populatedSet, data.futureDate);
+    }
+
+    @Test
+    public void testAllMeetingsReturned(){
+        List<Meeting> meetings = data.manager.getFutureMeetingList(selectedContact);
+        assertTrue(meetings.size() == 5);
+
+
+        List<Meeting> unSelectedMeetings = data.manager.getFutureMeetingList(unSelectedContact);
+        assertTrue(unSelectedMeetings.size() == 0);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNonExistContact(){
+        data.manager.getFutureMeetingList(nonExistContact);
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testNullContact(){
+        data.manager.getFutureMeetingList(null);
+    }
 }
